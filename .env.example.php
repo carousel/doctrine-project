029a2d80d12e464e672e1c21e6a218312b42f547
Miroslav Trninic <miroslav.trninic@gmail.com>
2017-01-26 13:44:03 +0100

    .env.example.php

---
+++ .env.example.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Example environment configuration
+ */
+return [
+    //env name
+    'name'     => "",
+    //mysql 
+    'dbname'   => "",
+    'user'     => "",
+    'password' => "",
+    'host'     => "",
+    'driver'   => "",
+    //file storage e.g. sqlite
+    'dbdriver' => "",
+    'dbpath'   => "",
+];
